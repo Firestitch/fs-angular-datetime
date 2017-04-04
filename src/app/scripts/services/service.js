e91dcf681742b9ceb54dac2c2d840de2368762a4
Raymond <ray@firestitch.com>
2017-04-04 10:47:55 -0400

    added moment()

--- src/app/scripts/services/service.js
+++ src/app/scripts/services/service.js
@@ -3,16 +3,39 @@
     angular.module('fs-angular-datetime')
 	.provider("fsDatetime",function() {
 
-		this.$get = function() {
+		this.$get = function(fsUtil) {
 
 			var service = {
-				parse: parse
+				parse: parse,
+				moment: _moment
 			 };
 
 			return service;
 
 			function parse(value) {
 				return Date.parse(value);
+			}
+
+			function _moment(value) {
+
+				if(moment.isMoment(value)) {
+					return value;
+				}
+
+				var date = undefined;
+				if(value instanceof Date) {
+					date = moment(value);
+				} else if(fsUtil.isString(value)) {
+					date = Date.parse(value);
+				} else if(fsUtil.isInt(value)) {
+					date = new Date(value);
+				}
+
+				if(date) {
+					return moment(date);
+				}
+
+				return date;
 			}
 		}
 	});
